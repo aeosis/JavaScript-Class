e4ffb05a59a8272abc14f6adf8e86b1d297fd986
aeosis <aeosis@users.noreply.github.com>
2017-06-27 18:34:48 -0500

    whatev

---
+++ class_4/Notes.js
@@ -0,0 +1,21 @@
+var i = 0;
+while (i<10) {
+	console.log(i);
+	i++;
+}
+
+
+for (let i=0; i<10; i++) {
+	console.log(i);
+}		
+
+
+function add(a,b) {
+	return a+b;
+}
+var result = add(5,3);	
+
+
+let myVar = "xy";
+
+//install JSTool Notepad++ plugin for auto formatting
